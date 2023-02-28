f2300550420afcdaf925d03d56d02af47fcc300f
Roger Pimenta <roger.pimenta@live.com>
2023-02-28 11:40:23 -0300

    Integração com Web APIs

--- alurapic/src/app/app.module.ts
+++ alurapic/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
-import { PhotoComponent } from './photos/photo/photo.component';
 import { PhotosModule } from './photos/photos.module';
 
 @NgModule({
--- alurapic/src/app/photos/photos.module.ts
+++ alurapic/src/app/photos/photos.module.ts
@@ -1,7 +1,9 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { PhotoComponent } from './photo/photo.component';
 
 @NgModule({
+  imports: [HttpClientModule],
   declarations: [PhotoComponent],
   exports: [PhotoComponent],
 })
